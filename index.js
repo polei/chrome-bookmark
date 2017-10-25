de29c22db6b0f8f93012f21e55b5f822d05fdc8e
陈双宇 <csy@chenshuangyudeMacBook-Pro.local>
2017-10-26 02:13:24 +0800

    文件导出

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ $iframe.on('load', function () {
             if ($a.length > 0) {
                 var url = $a.attr('href');
                 var name = $a.text();
-                var icon = $a.attr('icon');
+                var icon = $a.attr('icon')||'';
 
-                var $bookmarkItem = $('<div class="bookmark-item"><input type="checkbox" class="item-check"/><a href = "' + url + '" target="_blank"><img src="' + icon + '"/>' + name + '</a></div>');
+                var $bookmarkItem = $('<div class="bookmark-item"><input type="checkbox" class="item-check"/><a href = "' + url + '" target="_blank">'+(icon && ('<img src="' + icon + '"/>')||'' ) + name + '</a></div>');
                 $bookmarkItem.appendTo($target);
             }
             else {
@@ -46,3 +46,40 @@ $bookmarkContainer.on('click','input.group-check',function(){
 
     $(this).closest('.bookmark-group').find('input[type="checkbox"]').prop('checked',checked);
 });
+
+$("#btnExport").click(function(){
+    var $checkItems = $bookmarkContainer.find('input.item-check:checked');
+
+    var linkItemHtml = '';
+    $checkItems.each(function(index,item){
+        var $link = $(item).next('a');
+        var text = $link.text();
+        var href = $link.attr('href');
+        var icon = $link.find('img').attr('src')||'';
+
+        var iconHtml = (icon && ('ICON="' + icon + '"')) || '';
+        linkItemHtml += '<DT><A HREF="' + href + '" ' + iconHtml + '">' + text + '</A>\r\n';
+    });
+    
+    var headHtml = '\
+    <!DOCTYPE NETSCAPE-Bookmark-file-1>\r\n\
+    <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">\r\n\
+    <TITLE>Bookmarks</TITLE>\r\n\
+    <H1>Bookmarks</H1>\r\n\
+    <DL><p>\r\n\
+        <DT><H3 PERSONAL_TOOLBAR_FOLDER="true">书签栏</H3>\r\n\
+        <DL><p>\r\n';
+    var footHtml = '\
+        </DL><p>\r\n\
+    </DL><p>';
+
+    var preHtml = headHtml + linkItemHtml + footHtml;
+   
+    dataToTxt(preHtml,'导出书签.html');
+    return false;
+});
+
+function dataToTxt(content,fileName) {
+    var blob = new Blob([content], {type: "text/plain;charset=utf-8"});
+    saveAs(blob, fileName);//saveAs(blob,filename)
+}
